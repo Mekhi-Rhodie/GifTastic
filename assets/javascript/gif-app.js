ab44cafac32240b998b74ac07727d96e86f076f6
Mekhi-Rhodie <mekhirhodie@gmail.com>
2019-10-03 08:45:25 -0400

    Empty topics array.

--- assets/javascript/gif-app.js
+++ assets/javascript/gif-app.js
@@ -1,28 +1,4 @@
-var topics = [
-  "naruto",
-  "black clover",
-  "baka and test",
-  "bleach",
-  "dragon ball z",
-  "my hero academia",
-  "mob psycho 100",
-  "one punch man",
-  "highschool of the dead",
-  "heaven's lost property",
-  "attack on titan",
-  "soul eater",
-  "one peice",
-  "assassination classroom",
-  "fairy tail",
-  "gantz",
-  "full metal alchemist",
-  "keijo",
-  "kill la kill",
-  "akame ga kill",
-  "prison school",
-  "no game no life",
-  "sword art online"
-];
+var topics = [];
 
 $("#submit").on("click", function (event) {
   event.preventDefault();
