f91ebca7c1d7ad237235b452fdc3a8996b5fe190
Mekhi-Rhodie <mekhirhodie@gmail.com>
2019-10-03 07:20:55 -0400

    Change web protocol from "http" to "https".

--- assets/javascript/gif-app.js
+++ assets/javascript/gif-app.js
@@ -1,50 +1,54 @@
 var topics = [
-    "naruto",
-    "black clover",
-    "baka and test", 
-    "bleach", 
-    "dragon ball z", 
-    "my hero academia", 
-    "mob psycho 100", 
-    "one punch man", 
-    "highschool of the dead", 
-    "heaven's lost property", 
-    "attack on titan", 
-    "soul eater", 
-    "one peice", 
-    "assassination classroom", 
-    "fairy tail", 
-    "gantz", 
-    "full metal alchemist", 
-    "keijo", "kill la kill", 
-    "akame ga kill", 
-    "prison school", 
-    "no game no life", 
-    "sword art online"
+  "naruto",
+  "black clover",
+  "baka and test",
+  "bleach",
+  "dragon ball z",
+  "my hero academia",
+  "mob psycho 100",
+  "one punch man",
+  "highschool of the dead",
+  "heaven's lost property",
+  "attack on titan",
+  "soul eater",
+  "one peice",
+  "assassination classroom",
+  "fairy tail",
+  "gantz",
+  "full metal alchemist",
+  "keijo",
+  "kill la kill",
+  "akame ga kill",
+  "prison school",
+  "no game no life",
+  "sword art online"
 ];
 
-$("#submit").on("click", function(event){
+$("#submit").on("click", function (event) {
   event.preventDefault();
   var submission = $("#gif-choice").val().trim();
-  
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + submission + "&api_key=jZgvmAfsUyAWVLyCKuEAHj8keAQ2zpJ2&limit=15";
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      for(i = 0; i < response.data.length; i++){
-        console.log(response.data[i].images.original.url);
-        $("#gifs").append($("<img>").attr("src", response.data[i].images.original.url ));
-      }
-      //console.log(response.data)
-    });
-    $("#gif-buttons").append("<button>" + submission + "</button>")
+  topics.push(submission)
+
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + submission + "&api_key=jZgvmAfsUyAWVLyCKuEAHj8keAQ2zpJ2&limit=10";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    for (i = 0; i < response.data.length; i++) {
+      //console.log(response.data[i].images.original.url);
+      $("#gifs").prepend($("<img>").attr("src", response.data[i].images.original.url));
+    }
+    
+    console.log(response.data)
+  });
+
+$("#gif-buttons").append("<button>" + submission + "</button>")
+
+  for (i = 0; i < topics.length; i++) {
+    if (submission === topics[i]) {
+        
+    };
+  };
 });
-
-
-
-
-
-  
-
