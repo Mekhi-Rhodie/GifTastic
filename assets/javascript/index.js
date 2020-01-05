411433a4d7dbfedfe266f59560e921c12dac77da
Mekhi-Rhodie <mekhirhodie@gmail.com>
2020-01-05 02:58:11 -0500

    Add jQuery click event to display login modal

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,7 +1,8 @@
-$(document).ready(function(){
-    const email = $("#email").val().trim();
-    const pass = $("#password").val().trim();
-    $("#sign-up").on("click", function(){
-        $("#login-modal").fadeIn(600).css("display","block")
+const email = $("#email").val().trim();
+const pass = $("#password").val().trim();
+
+$(document).ready(function () {
+    $("#sign-up").on("click", function () {
+        $("#login-modal").fadeIn(600).css("display", "block")
     });
 });
